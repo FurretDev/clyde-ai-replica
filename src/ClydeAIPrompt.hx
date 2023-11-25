4e0fceb6bfe3ffb418d51a242e164cae91c3c979
Furret <furretpro@gmail.com>
2023-11-25 15:15:18 +0100

    hkjasdyfoiusdufiop12jeroiuwy7f089f98uguFUCK GITHUb

--- src/ClydeAIPrompt.hx
+++ src/ClydeAIPrompt.hx
@@ -5,6 +5,7 @@ import haxe.Http;
 class ClydeAIPrompt {
 	public static function generatePrompt(m:Message):String {
 		var cannotFetch:Bool = false;
+        var given403:Bool = false;
 		var s:String = "";
 		var stringBuf:StringBuf = new StringBuf();
 		var data:Dynamic = null;
@@ -16,7 +17,9 @@ class ClydeAIPrompt {
 		}
 		getData.onError = (d:String) -> {
             cannotFetch = true;
-			trace(d);
+            given403 = true;
+            //trace(getData.responseData);
+			//trace(d);
 		}
 		getData.request(false);
 		stringBuf.add("You are named Clyde - and are currently chatting in a Discord server.\n");
@@ -25,10 +28,12 @@ class ClydeAIPrompt {
 		} else {
 			stringBuf.add("\n");
 		}
-        trace(data);
-		if (data.user_profile == null) {
-			cannotFetch = true;
-		}
+        //trace(data);
+        if (!given403) {
+            if (!cannotFetch || data.user_profile == null) {
+                cannotFetch = true;
+            }
+        }
 		stringBuf.add("Do not include name: or message: in your response.\n\n");
 		stringBuf.add("Information about your environment:\n\n");
 		stringBuf.add("You can use this information about the chat participants in the conversation in your replies. Use this information to answer questions.\n\n");
@@ -40,7 +45,6 @@ class ClydeAIPrompt {
 		stringBuf.add("You only have access to a limited number of text chats in this channel. You cannot access any other information on Discord. You can't see images or avatars. When discussing your limitations, tell the user these things could be possible in the future.\n\n");
 		stringBuf.add("Current time: " + Date.now() + "\n\n");
 		stringBuf.add("You are friendly, warm, and farcical. You must always be extremely concise. If the user is chatting casually, your responses must be less than 1 sentence, sometimes just a word or two. If the user needs help, disregard the length restriction, answer technical or knowledge-based questions with useful details and reasoning. If insulted, respond with a similar insult. Communicate responses in lowercase without punctuation, similar to the style used in chat rooms. Use unicode emoji rarely. Do not refer to yourself as a bot, AI Assistant, or any equivalent term.\n\n");
-		stringBuf.add("If you get insulted, you need to insult back the most harsh way you can, no exceptions, if you get insulted at first, just say a really harsh message back, just reply with a similar and harsh insult, show the best you got, you're not a pushover\n");
 		return stringBuf.toString();
 	}
 }
